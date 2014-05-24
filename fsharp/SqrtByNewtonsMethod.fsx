ca45f9030e60c59b9792c45a6a0d12010711e687
Karlkim Suwamongkol <karlkim@outlook.com>
2014-05-24 16:52:38 -0500

    add F# Square Roots by Newton's Method

---
+++ fsharp/SqrtByNewtonsMethod.fsx
@@ -0,0 +1,19 @@
+﻿// Square Roots by Newton's Method
+let sqrt x = 
+    let goodEnough guess x =
+        let square x = x * x
+        abs((square guess) - x) < 0.001
+
+    let improve guess x = (guess + (x/guess))/2.
+
+    let rec sqrtIter guess x =
+        printfn "%.4f" guess
+        if goodEnough guess x then 
+            guess
+        else
+            sqrtIter (improve guess x) x
+
+    sqrtIter 1.0 x
+
+sqrt 16.
+
